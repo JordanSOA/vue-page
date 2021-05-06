c4c2fbc95680ad58458e8e73f15e7a25cbb1fed5
Jordan <48123323+JordanSOA@users.noreply.github.com>
2021-05-06 17:29:41 +0200

    minor fixes

--- front/personal-page/src/App.vue
+++ front/personal-page/src/App.vue
@@ -13,8 +13,8 @@
         color="primary"
         @click="openLink(social.icon)"
         text-color="white">
-      <v-icon >mdi-{{social.icon}}</v-icon>
-    </v-chip>
+          <v-icon >mdi-{{social.icon}}</v-icon>
+        </v-chip>
 
       <v-spacer></v-spacer>
         <v-checkbox class="pt-3"
@@ -93,41 +93,11 @@ export default {
 }
 
 .rubberBand {
-  -webkit-animation-name: rubberBand;
   animation-name: rubberBand;
-  -webkit-animation-duration: 1s;
   animation-duration: 1s;
-  -webkit-animation-fill-mode: both;
   animation-fill-mode: both;
   }
 
-
-  @-webkit-keyframes rubberBand {
-  0% , 100%{
-  -webkit-transform: scale3d(1, 1, 1);
-  transform: scale3d(1, 1, 1);
-  }
-  30% {
-  -webkit-transform: scale3d(1.25, 0.75, 1);
-  transform: scale3d(1.25, 0.75, 1);
-  }
-  40% {
-  -webkit-transform: scale3d(0.75, 1.25, 1);
-  transform: scale3d(0.75, 1.25, 1);
-  }
-  50% {
-  -webkit-transform: scale3d(1.15, 0.85, 1);
-  transform: scale3d(1.15, 0.85, 1);
-  }
-  65% {
-  -webkit-transform: scale3d(.95, 1.05, 1);
-  transform: scale3d(.95, 1.05, 1);
-  }
-  75% {
-  -webkit-transform: scale3d(1.05, .95, 1);
-  transform: scale3d(1.05, .95, 1);
-  }
-  }
 
   @keyframes rubberBand {
   0% , 100%{
