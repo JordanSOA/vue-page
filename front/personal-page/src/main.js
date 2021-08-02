91ea2cb10394973ba8430834c698c7e5fb97aeb4
Jordan <48123323+JordanSOA@users.noreply.github.com>
2021-08-02 18:29:44 +0200

    add router only for redirect to home whatever happens

--- front/personal-page/src/main.js
+++ front/personal-page/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import FlagIcon from 'vue-flag-icon';
 import i18n from './plugins/i18n'
+import router from '../src/router/index'
 
 Vue.use(FlagIcon);
 Vue.config.productionTip = false
@@ -10,5 +11,6 @@ Vue.config.productionTip = false
 new Vue({
   vuetify,
   i18n,
+  router,
   render: function (h) { return h(App) }
 }).$mount('#app')
