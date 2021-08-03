3903b247362672194413530015a0a9a503a996a3
Jordan <48123323+JordanSOA@users.noreply.github.com>
2021-08-03 22:16:10 +0200

    proper sentences, spacing done, might work on style

--- front/personal-page/src/components/FocusPoint.vue
+++ front/personal-page/src/components/FocusPoint.vue
@@ -1,16 +1,16 @@
 <template>
-    <v-card>
+    <v-card class="mb-3">
         <v-container>
             <v-row>
                 <v-list>
                     <v-list-item
-                    v-for="(item, index) in certif" :key="index">
+                    v-for="n in 3" :key="n">
                         <v-list-item-icon>
-                            <v-icon color="purple">mdi-star-four-points</v-icon>
+                            <v-icon color="primary">mdi-star-four-points</v-icon>
                         </v-list-item-icon>
                         <v-list-item-content >
                             <p class="text-center">
-                            {{item}}
+                            {{$t('focus_txt_'+n)}}
                             </p>
                         <!-- <v-list-item-title v-text="item"></v-list-item-title> -->
                         </v-list-item-content>
@@ -23,12 +23,7 @@
 
 <script>
 export default {
-    data: () => ({
-        certif: ["Software Engineer who enjoys building nifty and robust solutions",
-        "Can adapt and provide creative ideas to complex challenges",
-        "Connaissance sur des sujets comme Agile/Scrum, Software craftsmanship, TDD, CI/CD and DevOps.",
-        "Capable de concevoir et réaliser du back-end au front-end, de l’écriture des tests au déploiement en production."]
-    })
+    
 }
 </script>
 
--- front/personal-page/src/components/Home.vue
+++ front/personal-page/src/components/Home.vue
@@ -14,6 +14,7 @@
             <info-tabs/>
         </v-col>
     </v-row>
+
 </v-container>
 </template>
 
